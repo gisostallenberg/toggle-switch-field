94bd087775424e80079b54c0a559b0de4bb64b3e
Giso Stallenberg <gisostallenberg@users.noreply.github.com>
2025-04-29 10:11:31 +0200

    Allow JSON field toggle
    When the field to toggle is a json property, this could also be toggled

--- src/Http/Controllers/ToggleController.php
+++ src/Http/Controllers/ToggleController.php
@@ -1,7 +1,9 @@
 <?php
 namespace Naif\ToggleSwitchField\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Nova;
@@ -11,17 +13,27 @@ class ToggleController
     public function update(NovaRequest $request)
     {
         $resourceClass = Nova::resourceForKey($request->resource_name);
+        /** @var Model $resource */
         $resource = $resourceClass::newModel()->findOrFail($request->resource_id);
         $attribute = $request->input('attribute');
         $value = $request->input('new_value');
 
-        if (in_array($attribute, $resource->getFillable())) {
-            $resource->{$attribute} = $value;
+        $isFillable = in_array($attribute, $resource->getFillable());
+        if (Str::contains($attribute, '->')) {
+            $isFillable = in_array(Str::before($attribute, '->'), $resource->getFillable());
+        }
+
+        if (!$isFillable) {
+            return response()->json(['error' => 'Invalid attribute. Make sure column is a fillable field.'], 400);
+        }
+
+        $wasTimeStamped = $resource->timestamps;
+        $resource->setAttribute($attribute, $value);
+        try {
             $resource->timestamps = false;
             $resource->save();
-
-        } else {
-            return response()->json(['error' => 'Invalid attribute. Make sure column is a fillable field.'], 400);
+        } finally {
+            $resource->timestamps = $wasTimeStamped;
         }
     }
 }
